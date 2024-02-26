2bffce8f22acb6bcfe9c69a595aa29c20e1cad87
iftiaruddin240 <161312021+iftiaruddin240@users.noreply.github.com>
2024-02-26 19:38:15 +0600

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int integerType;
+    char charType;
+    float floatType;
+    double doubleType;
+
+    cout<<"Size of int is:"<<sizeof(integerType)<<endl;
+
+    cout<<"Size of char is:"<<sizeof(charType);
+    cout<<endl;
+    cout<<"Size of float is:"<<sizeof(float);
+    cout<<endl;
+    cout<<"Size of double is:"<<sizeof(double);
+    cout<<endl;
+
+    return 0;
+}
