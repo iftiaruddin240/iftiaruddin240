9a194e0618b0a62c7cf95139c984da876a5bd91c
iftiaruddin240 <161312021+iftiaruddin240@users.noreply.github.com>
2024-02-26 17:53:10 +0600

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string fullname;
+    cout<<"Enter your name:";
+    getline (cin,fullname);// this function allows to take input with spaces
+    cout<<"My name is "<<fullname<<endl;
+    return 0;
+}
