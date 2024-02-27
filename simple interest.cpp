11f5af3b10d6201b6be10247053e8a7d57eacb60
iftiaruddin240 <161312021+iftiaruddin240@users.noreply.github.com>
2024-02-27 23:41:59 +0600

    Add files via upload

---
+++ simple interest.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Calculate
+{
+public:
+    int a = 10;
+    int b = 20;
+    int addition()
+    {
+        cout<<a + b;
+
+    }
+};
+int main()
+{
+
+   Calculate sum;
+   sum.addition();
+}
